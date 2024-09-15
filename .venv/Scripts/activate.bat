e8b775c169a4d2efe199498aef461d98f7e41884
sinistersine <533991@student.fontys.nl>
2024-09-15 16:54:03 +0200

    Dit zijn mijn changes

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\annyo\Documents\Visual Code\DS5_GROUP_5\.venv
+set VIRTUAL_ENV=c:\Users\annyo\Documents\Visual Code\ds5_assignment_group5\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
