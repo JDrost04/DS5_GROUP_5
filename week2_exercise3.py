066c7568270deef623ef43d2d7d1f63160d4b6fc
Niels-Fontys <518107@student.fontys.nl>
2024-09-18 13:23:32 +0200

    networkx getest

--- week2_exercise3.py
+++ week2_exercise3.py
@@ -1,4 +1,9 @@
 #Opdracht 3
 import pandas as pd
 import math as m
-import networkx as net
+import networkx as nx
+import matplotlib.pyplot as plt
+
+NW = nx.star_graph(5)
+nx.draw(NW, with_labels=True)
+plt.show()
