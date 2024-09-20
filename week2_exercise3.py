299d40ee73b6b777b0eeeee4d1a293396bdfa5f0
lvSpaandonk <508422@student.fontys.nl>
2024-09-20 13:53:12 +0200

    goede versie van 3.1 (ieder punt apart gemaakt)

--- week2_exercise3.py
+++ week2_exercise3.py
@@ -9,25 +9,43 @@ N = 400
 M = 4
 n0 = 5
 
-def graph_generator(N,M,n0):
+#Opdracht 3.1 random versie
+import pandas as pd
+import math as m
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+'''
+N = 20
+M = 4
+n0 = 5
+'''
+
+def create_random_graph(N,M,n0):
     NW = nx.star_graph(n0)
-    newnodes = []
-    for i in range(N-6):
-       newnodes.append(i+6)
-    NW.add_nodes_from(newnodes)
-    for i in range(1,N):
-      if len(list(nx.all_neighbors(NW,i))) <M:
-           for j in range((N-1)-i):
-               if len(list(nx.all_neighbors(NW,N-(j+1)))) <M:
-                   NW.add_edge(i,N-(j+1))
-               if len(list(nx.all_neighbors(NW,i))) ==M:
-                  break
+    node_amount = n0+1
+    print('yep de werkt nog')
+    for i in range(n0+1,N):
+        pagerank_chances = nx.pagerank(NW)
+        NW.add_node(i)
+        link_points = np.random.choice(node_amount,M,replace=False,p=list(pagerank_chances.values()))
+        for j in range(M):
+            NW.add_edge(i,link_points[j])
+        node_amount += 1
+        print(f'je bent nu bij node {node_amount}')
+    nx.draw(NW)
+    plt.show()
     return NW
 
-nx.draw(graph_generator(N,M,n0))
+
+
+
+
+
+nx.draw(create_random_graph(N,M,n0))
 plt.show()
 
-para = nx.pagerank(graph_generator(N,M,n0))
+para = nx.pagerank(create_random_graph(N,M,n0))
 df3 = pd.DataFrame.from_dict(para, orient='index', columns=['Pagerank'])
 print(df3)
 
