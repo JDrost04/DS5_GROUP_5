88f99e8fcc1b8b0e4bc58a3866fe3be74e19f688
Niels-Fontys <518107@student.fontys.nl>
2024-09-20 14:04:04 +0200

    Laatste cleanup voor docstring

--- week2_exercise3.py
+++ week2_exercise3.py
@@ -1,30 +1,19 @@
-#Opdracht 3
+#Opdracht 3.1
 import pandas as pd
 import math as m
 import networkx as nx
 import matplotlib.pyplot as plt
 import scipy.stats as sp
+import numpy as np
 
 N = 400
 M = 4
 n0 = 5
 
-#Opdracht 3.1 random versie
-import pandas as pd
-import math as m
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy as np
-'''
-N = 20
-M = 4
-n0 = 5
-'''
-
 def create_random_graph(N,M,n0):
     NW = nx.star_graph(n0)
     node_amount = n0+1
-    print('yep de werkt nog')
+    #print('yep de werkt nog')
     for i in range(n0+1,N):
         pagerank_chances = nx.pagerank(NW)
         NW.add_node(i)
@@ -32,11 +21,8 @@ def create_random_graph(N,M,n0):
         for j in range(M):
             NW.add_edge(i,link_points[j])
         node_amount += 1
-        print(f'je bent nu bij node {node_amount}')
+        #print(f'je bent nu bij node {node_amount}')
     return NW
-
-
-
 
 NW = create_random_graph(N,M,n0)
 
@@ -48,10 +34,8 @@ df3 = pd.DataFrame.from_dict(para, orient='index', columns=['Pagerank'])
 print(df3)
 
 
-
-
 #opdracht 3.2
-df = pd.read_csv('ds5_assignment_group5/squirrel_edges.csv')
+df = pd.read_csv('squirrel_edges.csv')
 df.columns = ['from', 'to']
 G = nx.DiGraph()
 G.add_nodes_from(df.loc[:,'from'])
